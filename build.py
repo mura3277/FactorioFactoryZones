50b967f545ce82a8410ea8515db20cc494103407
Muramasa <haydenkilloh@gmail.com>
2026-08-31 03:37:49 +0100

    fix: build script includes correct archive sub folder and is mod name agnostic

--- build.py
+++ build.py
@@ -1,20 +1,20 @@
-import json, sys, shutil, pathlib, os
+import json, sys, shutil, pathlib, os, zipfile
 
 ROOT_PATH = pathlib.Path(__file__).resolve().parent
-INFO_JSON = ROOT_PATH / "factory_zones/info.json"
-TARGET_FOLDER = str(ROOT_PATH / "factory_zones")
+INFO_JSON = ROOT_PATH / "data/info.json"
+TARGET_FOLDER = ROOT_PATH / "data"
 
 if not INFO_JSON.exists(): sys.exit(f"ERROR: info.json not found")
 
 with open(INFO_JSON, "r", encoding="utf-8") as f: info = json.load(f)
 
-mod_name = info.get("name")
-mod_version = info.get("version")
+file_name = f"{info.get("name")}_{info.get("version")}"
+archive_path = ROOT_PATH / f"{file_name}.zip"
 
-if not mod_name: sys.exit('ERROR: info.json has no "name" field.')
-if not mod_version: sys.exit('ERROR: info.json has no "version" field.')
+with zipfile.ZipFile(archive_path, "w", zipfile.ZIP_DEFLATED) as zf:
+    for path in TARGET_FOLDER.rglob("*"):
+        # construct zip with parent folder containing all files as required by factorio mods
+        zf.write(path, pathlib.Path(file_name) / path.relative_to(TARGET_FOLDER))
 
-archive_path = str(ROOT_PATH / f"{mod_name}_{mod_version}")
-
-shutil.make_archive(archive_path, "zip", TARGET_FOLDER)
-shutil.copy(f"{archive_path}.zip", str(os.path.join(os.getenv('APPDATA'), "Factorio\\mods")))
+# move new archive to appdata factorio mods folder
+shutil.copy(archive_path, str(os.path.join(os.getenv('APPDATA'), "Factorio\\mods")))
