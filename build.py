f8d54c4984274981a252a6a55683c892228de31a
Muramasa <haydenkilloh@gmail.com>
2026-08-31 02:42:31 +0100

    initial commit + build script

---
+++ build.py
@@ -0,0 +1,17 @@
+import json, sys, shutil, pathlib
+
+ROOT_PATH = pathlib.Path(__file__).resolve().parent
+INFO_JSON = ROOT_PATH / "factory_zones/info.json"
+TARGET_FOLDER = str(ROOT_PATH / "factory_zones")
+
+if not INFO_JSON.exists(): sys.exit(f"ERROR: info.json not found")
+
+with open(INFO_JSON, "r", encoding="utf-8") as f: info = json.load(f)
+
+mod_name = info.get("name")
+mod_version = info.get("version")
+
+if not mod_name: sys.exit('ERROR: info.json has no "name" field.')
+if not mod_version: sys.exit('ERROR: info.json has no "version" field.')
+
+shutil.make_archive(str(ROOT_PATH / f"{mod_name}_{mod_version}"), "zip", TARGET_FOLDER)
